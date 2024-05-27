13d501de64a34f167d579160f993156d83041a53
Sergii Rudniev <100538048+ONETAPL3G3ND@users.noreply.github.com>
2024-05-27 21:39:44 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import threading
 import time
-import tokengraber
 import asyncio
 import os
 import logging
 import DiscordManager
 import LaunchingUkrainizaciya
+# ---------------------ProxySetup---------------------
+import winreg as reg
+
+# ----------------------------------------------------
+# ---------------------tokengraber---------------------
+import time
+
+from mitmproxy import options
+from mitmproxy.tools.dump import DumpMaster
+import asyncio
+import logging
+from mitmproxy.addonmanager import Loader
+
+# -----------------------------------------------------
 
 logging.basicConfig(
     level=logging.INFO,
@@ -16,12 +29,13 @@ logging.basicConfig(
     ]
 )
 logger = logging.getLogger(__name__)
-#---------------------ProxySetup---------------------
-import winreg as reg
 
+
+# ---------------------ProxySetup---------------------
 class ProxySetup:
     def __init__(self):
         ...
+
     @classmethod
     def set_proxy(cls, proxy) -> None:
         internet_settings = reg.OpenKey(reg.HKEY_CURRENT_USER,
@@ -41,7 +55,79 @@ class ProxySetup:
         reg.SetValueEx(internet_settings, 'ProxyEnable', 0, reg.REG_DWORD, 0)
 
         reg.CloseKey(internet_settings)
-#--------------------------------------------------------
+
+
+# --------------------------------------------------------
+
+# ---------------------tokengraber---------------------
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("requests.log"),
+        logging.StreamHandler()
+    ]
+)
+logger = logging.getLogger(__name__)
+finished = False
+
+
+class RequestLogger:
+    def load(self, loader: Loader):
+        loader.add_option(
+            name="listen_host", typespec=str, default="127.0.0.1",
+            help="Proxy listen address"
+        )
+        loader.add_option(
+            name="listen_port", typespec=int, default=8080,
+            help="Proxy listen port"
+        )
+
+    def request(self, flow):
+        global finished
+        request = flow.request
+        for header, value in request.headers.items():
+            if header == "authorization" and "discord.com" in request.url:
+                logger.info("---------------------------------------")
+                logger.info(f"DETECTED TOKEN DISCORD: {value}")
+                logger.info("---------------------------------------")
+                with open("token.txt", "w") as file:
+                    file.write(value)
+                finished = True
+            else:
+                logger.info(f"URL: {flow.request.url}")
+                logger.info("Request Headers:")
+                for key, value in flow.request.headers.items():
+                    logger.info(f"{key}: {value}")
+                logger.info("\n")
+
+
+class tokengraber:
+    @classmethod
+    async def start(cls):
+        global finished
+        opts = options.Options(listen_host='127.0.0.1', listen_port=8080)
+        pconf = opts.keys()
+        m = DumpMaster(opts)
+
+        m.addons.add(RequestLogger())
+
+        try:
+            asyncio.create_task(m.run())
+            while finished == False:
+                await asyncio.sleep(1)
+            logger.info("Closing TokenGraber Proxy")
+            m.shutdown()
+
+
+        except SystemExit:
+            ...
+
+
+# --------------------------------------------------------
+
+# ---------------------StartPoint-------------------------
 
 async def main():
     logger.info("Proxy installation.")
